061c28291b155edbedd4b0cb197df7eb614cd156
Alex <emerald2014@mail.ru>
2022-03-07 16:05:11 +0300

    Реализовал сортировку по позиции

--- app/src/main/java/ru/kudesnik/infograce/LayerFragment.kt
+++ app/src/main/java/ru/kudesnik/infograce/LayerFragment.kt
@@ -2,21 +2,22 @@ package ru.kudesnik.infograce
 
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.transition.AutoTransition
-import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ListAdapter
-import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.Callback.makeMovementFlags
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ru.kudesnik.infograce.databinding.FragmentLayerBinding
 
+//Задачи
+/*1. Перетаскивание элементов списка
+2. Сортировка списка по позиции, а не по порядку
+3. Сохранение в SharedPreference позиции, и переменных*/
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val LAYER_SETTINGS = "layer_settings"
@@ -42,11 +43,25 @@ open class LayerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val valuesString = listOf<String>(
-            "Android", "iPhone", "WindowsMobile",
+            "Слой", "iPhone", "WindowsMobile",
             "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
             "Linux", "OS/2"
         )
-
+        val items: List<Item> = listOf<Item>(
+            Item(0, "Слой делян", 2, false, 65),
+            Item(1, "Сигналы о лесоизменениях, тестовая выборка с ув-ным шагом", 1, true, 26),
+            Item(2, "Преграды для прохождения огня", 0, true, 0),
+//            Item(3, "Маска облачности от 02.07.2021", 3),
+//            Item(4, "Маска облачности от 02.07.2021", 4),
+//            Item(5, "Папка со слоями", 5),
+//            Item(6, "Сигналы о лесоизменениях, тестовая выборка с ув-ным шагом", 6),
+//            Item(7, "Преграды для прохождения огня", 7),
+//            Item(8, "Контуры гарей", 8),
+//            Item(9, "Маска облачности от 02.07.2021", 9),
+//            Item(10, "Маска облачности от 02.07.2021", 10),
+//            Item(11, "Маска облачности от 02.07.2021", 1),
+//            Item(12, "Маска облачности от 02.07.2021", 12),
+        )
 
 //        val adapter:ListAdapter =
 //            ArrayAdapter(requireContext(), R.layout.item_layer, R.id.itemName, valuesString)
@@ -67,13 +82,12 @@ open class LayerFragment : Fragment() {
 //            val arrow = included.arrowButton
 //            val hiddenView = included.hiddenView
 //            val listView2 = listView
-            val recyclerView: RecyclerView = recyclerView
+            val rw: RecyclerView = recyclerView
             recyclerView.layoutManager = LinearLayoutManager(requireContext())
-            recyclerView.adapter = LayerFragmentAdapter(valuesString)
+            recyclerView.adapter = LayerFragmentAdapter(items, requireContext())
 
 //            listView2.setBackgroundColor(resources.getColor(R.color.purple_200))
 //            listView.adapter = adapter
-
 
 
 //            arrow.setOnClickListener {
@@ -123,51 +137,110 @@ open class LayerFragment : Fragment() {
 //            button3.setOnClickListener {
 //                Toast.makeText(requireContext(), "Нажата кнопка 3", Toast.LENGTH_SHORT).show()
 //            }
+            setupSwipeListener(rw)
+
+
+
+            setupClicks()
         }
     }
 
+    private fun setupClicks() =
+        adapter?.setOnItemClickListener(object : LayerFragmentAdapter.OnItemClickListener {
+            override fun onItemClick(view: View?, position: Int) {
+                Toast.makeText(requireContext(), "Click $position", Toast.LENGTH_SHORT).show()
 
-    private fun setCurrentSlider(sliderValue: Int) {
-        val sharedPreferences: SharedPreferences = requireActivity().getSharedPreferences(
-            LAYER_SETTINGS, AppCompatActivity.MODE_PRIVATE
-        )
-        val editor: SharedPreferences.Editor = sharedPreferences.edit()
-        editor.putInt(SLIDER_VALUE, sliderValue)
-        editor.apply()
+            }
+
+            override fun onItemLongClick(view: View?, position: Int) {
+                Toast.makeText(requireContext(), "Long Click $position", Toast.LENGTH_SHORT).show()
+
+            }
+        })
+
+
+    private fun setupSwipeListener(rw: RecyclerView) {
+        val callback = object : ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.UP
+        ) {
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val item = adapter?.itemList?.get(viewHolder.adapterPosition)
+                Toast.makeText(requireContext(), "Swipe $item", Toast.LENGTH_SHORT).show()
+//                viewModel.deleteShopItem(item)
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(callback)
+        itemTouchHelper.attachToRecyclerView(rw)
     }
+        fun getMovementFlags(
+            recyclerView: RecyclerView?,
+            viewHolder: RecyclerView.ViewHolder?
+        ): Int {
+            val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
+            val swipeFlags = ItemTouchHelper.START or ItemTouchHelper.END
+            return makeMovementFlags(dragFlags, swipeFlags)
+        }
 
-    private fun getCurrentSlider(): Int {
-        val sharedPreferences: SharedPreferences = requireActivity().getSharedPreferences(
-            LAYER_SETTINGS, AppCompatActivity.MODE_PRIVATE
-        )
-        return (sharedPreferences.getInt(SLIDER_VALUE, 0))
-    }
+        fun isLongPressDragEnabled(): Boolean {
+            return true
+        }
 
-    private fun setCurrentSwitch(switchIsChecked: Boolean) {
-        val sharedPreferences: SharedPreferences = requireActivity().getSharedPreferences(
-            LAYER_SETTINGS, AppCompatActivity.MODE_PRIVATE
-        )
-        val editor: SharedPreferences.Editor = sharedPreferences.edit()
-        editor.putBoolean(SWITCH_CHECKED, switchIsChecked)
-        editor.apply()
-    }
+        fun isItemViewSwipeEnabled(): Boolean {
+            return true
+        }
 
-    private fun getCurrentSwitch(): Boolean {
-        val sharedPreferences: SharedPreferences = requireActivity().getSharedPreferences(
-            LAYER_SETTINGS, AppCompatActivity.MODE_PRIVATE
-        )
-        return (sharedPreferences.getBoolean(SWITCH_CHECKED, true))
-    }
+        fun setCurrentSlider(sliderValue: Int) {
+            val sharedPreferences: SharedPreferences = requireActivity().getSharedPreferences(
+                LAYER_SETTINGS, AppCompatActivity.MODE_PRIVATE
+            )
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putInt(SLIDER_VALUE, sliderValue)
+            editor.apply()
+        }
 
-    companion object {
+        fun getCurrentSlider(): Int {
+            val sharedPreferences: SharedPreferences = requireActivity().getSharedPreferences(
+                LAYER_SETTINGS, AppCompatActivity.MODE_PRIVATE
+            )
+            return (sharedPreferences.getInt(SLIDER_VALUE, 0))
+        }
 
-        fun newInstance() = LayerFragment()
+        fun setCurrentSwitch(switchIsChecked: Boolean) {
+            val sharedPreferences: SharedPreferences = requireActivity().getSharedPreferences(
+                LAYER_SETTINGS, AppCompatActivity.MODE_PRIVATE
+            )
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putBoolean(SWITCH_CHECKED, switchIsChecked)
+            editor.apply()
+        }
 
-    }
-    private fun fillList(): List<String> {
-        val data = mutableListOf<String>()
-        (0..30).forEach { i -> data.add("$i element") }
-        return data
-    }
+        fun getCurrentSwitch(): Boolean {
+            val sharedPreferences: SharedPreferences = requireActivity().getSharedPreferences(
+                LAYER_SETTINGS, AppCompatActivity.MODE_PRIVATE
+            )
+            return (sharedPreferences.getBoolean(SWITCH_CHECKED, true))
+        }
+
+        companion object {
+
+            fun newInstance() = LayerFragment()
+
+        }
+
 
 }
+
+    interface ItemTouchHelperAdapter {
+        fun onItemMove(fromPosition: Int, toPosition: Int)
+        fun onItemDismiss(position: Int)
+    }
--- app/src/main/java/ru/kudesnik/infograce/LayerFragmentAdapter.kt
+++ app/src/main/java/ru/kudesnik/infograce/LayerFragmentAdapter.kt
@@ -1,43 +1,107 @@
 package ru.kudesnik.infograce
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.Typeface
 import android.transition.AutoTransition
 import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import ru.kudesnik.infograce.databinding.ItemLayerBinding
+import java.util.*
 
-class LayerFragmentAdapter(private val names: List<String>) :
+
+private const val LAYER_SETTINGS = "layer_settings"
+private const val SWITCH_CHECKED = "switch_checked"
+private const val SLIDER_VALUE_0 = "SLIDER_VALUE"
+private const val SLIDER_VALUE_1 = "SLIDER_VALUE"
+private const val SLIDER_VALUE_2 = "SLIDER_VALUE"
+
+class LayerFragmentAdapter(private val items: List<Item>, private val context: Context) :
     RecyclerView.Adapter<LayerFragmentAdapter.MyViewHolder>() {
     private lateinit var binding: ItemLayerBinding
 
+    //    private lateinit var context:Context
+    private var sliderConst = ""
 
-  inner  class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(name: String) = with(binding) {
-itemName.text = name
-imageItem.load(R.drawable.item_temp)
-            //            val cardView = included.baseCardView
+    var itemList = listOf<Item>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: Item, context: Context) = with(binding) {
+            itemName.text = item.name
+            imageItem.load(R.drawable.item_temp)
+
+//Slider
+            val slider = slider
+            slider.value = getCurrentSlider(item.id).toFloat()
+            textViewItemPercent.text = slider.value.toInt().toString()
+            val textItemPercent = textViewItemPercent
+            slider.addOnChangeListener { slider, value, fromUser ->
+                setCurrentSlider(value.toInt(), item.id)
+                textItemPercent.text = value.toInt().toString()
+            }
+//Switch
+            val switch = switchItem
+            switchItem.isChecked = getCurrentSwitch()
+
+            switch.setOnCheckedChangeListener { buttonView, isChecked ->
+                val check = if (isChecked) "включен" else "выключен"
+
+                Toast.makeText(context, "Переключатель $check", Toast.LENGTH_SHORT).show()
+                setCurrentSwitch(isChecked)
+            }
+//Buttons
+            val button1 = itemButton1
+            button1.setOnClickListener {
+                Toast.makeText(context, "Нажата кнопка 1", Toast.LENGTH_SHORT).show()
+            }
+            val button2 = itemButton2
+            button2.setOnClickListener {
+                Toast.makeText(context, "Нажата кнопка 2", Toast.LENGTH_SHORT).show()
+            }
+            val button3 = itemButton3
+            button3.setOnClickListener {
+                Toast.makeText(context, "Нажата кнопка 3", Toast.LENGTH_SHORT).show()
+            }
+
+//Root нажатие
             root.setOnClickListener {
+
 //                Toast.makeText(require(), "ToastInFragment", Toast.LENGTH_SHORT).show()
                 if (hiddenView.visibility == View.VISIBLE) {
                     TransitionManager.beginDelayedTransition(baseCardView, AutoTransition())
+                    itemName.setTypeface(null, Typeface.NORMAL)
+                    itemName.setTextColor(context.resources.getColor(R.color.white))
                     hiddenView.visibility = View.GONE
                     arrowButton.setImageResource(R.drawable.ic_baseline_keyboard_arrow_down_24)
                 } else {
                     TransitionManager.beginDelayedTransition(
                         baseCardView, AutoTransition()
                     )
+                    itemName.setTypeface(null, Typeface.BOLD)
+                    itemName.setTextColor(context.resources.getColor(R.color.item_text_bold))
                     hiddenView.visibility = View.VISIBLE
                     arrowButton.setImageResource(R.drawable.ic_baseline_keyboard_arrow_up_24)
-                } }
+                }
             }
 
+//Root LongClick
+            root.setOnLongClickListener {
+                Toast.makeText(context, "Long Click ", Toast.LENGTH_SHORT).show()
+                true
+            }
         }
+    }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -51,8 +115,112 @@ imageItem.load(R.drawable.item_temp)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.bind(names[position])
+
+        holder.bind(items.sortedBy { it.position }[position], context = context)
+
+        setClickListener(holder);
     }
 
-    override fun getItemCount(): Int = names.size
+    // Настраиваем мониторинг кликов
+    private fun setClickListener(viewHolder: MyViewHolder) {
+        if (mOnItemClickListener != null) {
+
+            // Настраиваем слушателя через View's setOnClickListener
+            viewHolder.itemView.setOnClickListener {
+                mOnItemClickListener!!.onItemClick(
+                    viewHolder.itemView,
+                    viewHolder.layoutPosition
+                )
+
+            }
+            viewHolder.itemView.setOnLongClickListener {
+                mOnItemClickListener!!.onItemLongClick(
+                    viewHolder.itemView,
+                    viewHolder.layoutPosition
+                )
+                false
+            }
+        }
+    }
+
+    // интерфейс события щелчка по элементу, обратный вызов Activity
+    interface OnItemClickListener {
+        fun onItemClick(view: View?, position: Int)
+        fun onItemLongClick(view: View?, position: Int)
+    }
+
+    private var mOnItemClickListener: OnItemClickListener? = null
+
+    // Вызывается в Activity
+    fun setOnItemClickListener(listener: OnItemClickListener?) {
+        mOnItemClickListener = listener
+    }
+
+    override fun getItemCount(): Int = items.size
+
+
+    private fun setCurrentSlider(sliderValue: Int, position: Int) {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences(
+            LAYER_SETTINGS, AppCompatActivity.MODE_PRIVATE
+        )
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        when (position) {
+            0 -> sliderConst = SLIDER_VALUE_0
+            1 -> sliderConst = SLIDER_VALUE_1
+            2 -> sliderConst = SLIDER_VALUE_2
+        }
+        editor.putInt(sliderConst, sliderValue)
+        editor.apply()
+    }
+
+    private fun getCurrentSlider(position: Int): Int {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences(
+            LAYER_SETTINGS, AppCompatActivity.MODE_PRIVATE
+        )
+        when (position) {
+            0 -> sliderConst = SLIDER_VALUE_0
+            1 -> sliderConst = SLIDER_VALUE_1
+            2 -> sliderConst = SLIDER_VALUE_2
+        }
+        return (sharedPreferences.getInt(sliderConst, 0))
+    }
+
+    private fun setCurrentSwitch(switchIsChecked: Boolean) {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences(
+            LAYER_SETTINGS, AppCompatActivity.MODE_PRIVATE
+        )
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putBoolean(SWITCH_CHECKED, switchIsChecked)
+        editor.apply()
+    }
+
+    private fun getCurrentSwitch(): Boolean {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences(
+            LAYER_SETTINGS, AppCompatActivity.MODE_PRIVATE
+        )
+        return (sharedPreferences.getBoolean(SWITCH_CHECKED, true))
+    }
+
+//    override fun onItemMove(fromPosition: Int, toPosition: Int): Boolean {
+//        if (fromPosition < toPosition) {
+//            for (i in fromPosition until toPosition) {
+//                Collections.swap(items, i, i + 1)
+//            }
+//        } else {
+//            for (i in fromPosition downTo toPosition + 1) {
+//                Collections.swap(items, i, i - 1)
+//            }
+//        }
+//        notifyItemMoved(fromPosition, toPosition)
+//        return true
+//    }
+
+//    val items2 = listOf<String>("1", "2")
+//
+//    override fun onItemDismiss(position: Int) {
+//        items2.remove(position);
+//        notifyItemRemoved(position);
+//    }
 }
+
+
